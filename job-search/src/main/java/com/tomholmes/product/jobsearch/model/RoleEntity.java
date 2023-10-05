ef61dcaca89e651f71901069c0d4b8f18497e896
tjholmes66 <tom@tomholmes.net>
2023-10-05 15:33:52 -0400

    test commit 2

--- job-search/src/main/java/com/tomholmes/product/jobsearch/model/RoleEntity.java
+++ job-search/src/main/java/com/tomholmes/product/jobsearch/model/RoleEntity.java
@@ -1,6 +1,26 @@
 package com.tomholmes.product.jobsearch.model;
 
-public class RoleEntity
-{
+import java.io.Serializable;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "user")
+public class RoleEntity implements Serializable
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long Id;
+
+    @Column(name = "enabled")
+    private boolean enabled;
+    
+    
 }
