e9dca9705ce95fa752aeebaab1330eb9a13fff2b
ryankahil <ryan.kahil@outlook.com>
2026-08-28 10:15:51 -0400

    Removing unnecesary user_provisioning.sql script

--- provision_users.sql
+++
@@ -1,8 +0,0 @@
--- Create the missing localhost user profile your Mac IDE is demanding
-CREATE USER IF NOT EXISTS 'job_search_user'@'localhost' IDENTIFIED BY 'passw0rd';
-
--- Grant full schema and flyway metadata access to the localhost profile
-GRANT ALL PRIVILEGES ON job_search_db.* TO 'job_search_user'@'localhost';
-GRANT SELECT, REPLICATION CLIENT ON *.* TO 'job_search_user'@'localhost';
-
-FLUSH PRIVILEGES;
